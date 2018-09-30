c6b26db6ef059808a9c6bde65d11709263752f65
manoj <manoj38.mk@gmail.com>
2018-10-01 00:57:20 +0530

    added maven project to github workspace

---
+++ src/main/java/jar/second.java
@@ -0,0 +1,13 @@
+package jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class second
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Devops practice" );
+    }
+}
